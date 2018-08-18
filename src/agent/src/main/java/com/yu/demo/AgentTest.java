44f76e67f06a00f8264b3e5a59101fbd6ebb648f
yu <823947488@qq.com>
2018-08-18 17:36:21 +0800

    这是李四开发完成Agent模块首次提交

---
+++ src/agent/src/main/java/com/yu/demo/AgentTest.java
@@ -0,0 +1,17 @@
+package com.yu.demo;
+
+/**
+ * Created by yu on 2018/8/18 17:17.
+ */
+public class AgentTest {
+    /**
+     * 这是李四开发的Agent模块，
+     */
+    public static void main(String[] args) {
+        System.out.println("hello Agent");
+    }
+
+    //但是李四下一次会区修改张三的模块应该会发生冲突，提交自己的模块不会冲突
+
+
+}
